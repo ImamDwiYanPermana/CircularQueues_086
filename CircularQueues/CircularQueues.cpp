62c04b48bb111f878fa38932039983e3f5cf41b2
ImamDwiYanPermana <imampermana224@gmail.com>
2023-06-23 14:46:25 +0700

    menambahkan class queues yang berisi FRONT REAR dan MAX

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 
-class queues{};
+class queues{
+    int FRONt, REAR, MAX = 5; 
+    int queues_array[5];
+};
 int main()
 {
     std::cout << "Hello World!\n";
