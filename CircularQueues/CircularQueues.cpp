26d2965a44cec5c20c8c3618acf4ca78301a8559
ImamDwiYanPermana <imampermana224@gmail.com>
2023-06-23 15:52:23 +0700

    memnaggil iterasi dari front hingga rear

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -66,7 +66,15 @@ void display() {
 
         cout << "/nElement in the queue are....\n";
 
-  
+        //Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR 
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queues_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+      
 };
 
 int main()
