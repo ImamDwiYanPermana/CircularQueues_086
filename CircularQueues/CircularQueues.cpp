0a4fb053202732d6f83e35b10803ab0acd99ee8f
ImamDwiYanPermana <imampermana224@gmail.com>
2023-06-23 15:50:09 +0700

    menambhakan void display dan menambhakna variable int

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -54,7 +54,18 @@ public:
                 FRONT = FRONT + 1;
         }
     }
+void display() {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
 
+       
 };
 
+
+
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
 
