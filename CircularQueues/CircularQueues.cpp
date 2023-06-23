ffa3e9af2a4402cbd74f201b0ceb88f20682a549
ImamDwiYanPermana <imampermana224@gmail.com>
2023-06-23 14:48:43 +0700

    MENAMBAHKAN KONSTRUKTOR

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 class queues{
-    int FRONt, REAR, MAX = 5; 
+    int FRONT, REAR, MAX = 5; // elment di ganti menjadi MAX
     int queues_array[5];
+public:
+    queues() {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+
 };
 int main()
 {
